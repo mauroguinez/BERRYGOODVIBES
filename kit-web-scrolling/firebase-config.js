3005080c4e273e93f26c172e6e6207fcba0994f0
mauroguinez <guinezmauro7@gmail.com>
2026-08-07 21:20:19 -0300

    Delete kit-web-scrolling directory

--- kit-web-scrolling/firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAG1MCRBj_mI9uA1RG0jTN6zD5LfCPwRzY",
-  authDomain: "berrygood-2ddd2.firebaseapp.com",
-  projectId: "berrygood-2ddd2",
-  storageBucket: "berrygood-2ddd2.firebasestorage.app",
-  messagingSenderId: "645618848175",
-  appId: "1:645618848175:web:f18e37b566ef33b9a19f5a",
-  measurementId: "G-F76V6QMN01"
-};
